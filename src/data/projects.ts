05b2b97246dca7413adecc01976b40ea0cde1f83
Satriadaffaaa <satriadaffa878@gmail.com>
2025-10-23 22:19:09 +0700

    commit

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -20,7 +20,7 @@ export const projects: Project[] = [
     title: "Event Management",
     category: "Web App / UI/UX Design",
     description: "Designing a web-based event management system for DPMPTSP, from user flows to high-fidelity prototypes.",
-    image: "https://postimg.cc/MXdhj9Kw",
+    image: "https://postimg.cc/XrQGMSnG",
     additionalImages: [
       "Public/Images/Projects/EventManagement/Dashboard.png",
       "Public/Images/Projects/EventManagement/Homepage2.png",
